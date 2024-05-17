0a91a7ae7de5448e9afd512ebeb7f8270967d96a
davidalpha <david.alpha@gmail.com>
2024-05-17 19:01:45 +0700

    fixed all platform types, removed some unused assets and scripts

--- scripts/object_move_and_ferry.gd
+++ scripts/object_move_and_ferry.gd
@@ -2,23 +2,32 @@ extends PathFollow3D
 
 @export var speed: float = 3.0
 @export var pauseTimeAtBeginAndEnd: float = 1.0
-var move_direction: int = -1
+var move_direction: int = 1
+var moving: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var timer = get_tree().create_timer(pauseTimeAtBeginAndEnd)
-	timer.timeout.connect(toggle_direction)
+	timer.timeout.connect(start_moving)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(move_direction)
-	progress_ratio += (delta*(speed/100))*move_direction
-	if progress_ratio == 0:
-		var timer = get_tree().create_timer(pauseTimeAtBeginAndEnd)
-		timer.timeout.connect(toggle_direction)
-	if progress_ratio == 1:
-		var timer = get_tree().create_timer(pauseTimeAtBeginAndEnd)
-		timer.timeout.connect(toggle_direction)
+	if moving:
+		progress_ratio += (delta*(speed/100))*move_direction
 		
-func toggle_direction():
-	move_direction = move_direction * - 1
+	if progress_ratio == 0:
+		moving = false
+		var timer = get_tree().create_timer(pauseTimeAtBeginAndEnd)
+		timer.timeout.connect(start_moving)
+		move_direction = 1
+		
+	if progress_ratio >= 1:
+		moving = false
+		var timer = get_tree().create_timer(pauseTimeAtBeginAndEnd)
+		timer.timeout.connect(start_moving)
+		move_direction = -1
+		
+
+
+func start_moving():
+	moving = true
